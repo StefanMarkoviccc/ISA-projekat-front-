ec8d0cf246514b8bfa4ad5fadb921f618f1ddae1
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-11-23 07:45:13 +0100

    #povezivanje front:

---
+++ src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  
+  url = "http://localhost:8080";
+
+
+  houseOwnerRegistration(data: any) {
+    return this.http.post(this.url + '/api/users/register/...', data);
+  }
+}
+
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-registration',
@@ -10,7 +12,7 @@ export class RegistrationComponent implements OnInit {
   form: FormGroup;
   selectedType: any;
 
-  constructor(private formBuilder: FormBuilder) 
+  constructor(private formBuilder: FormBuilder, private api: ApiService, private router: Router) 
   {
     this.form = this.formBuilder.group({
       email: ['', Validators.email],
@@ -32,6 +34,23 @@ export class RegistrationComponent implements OnInit {
 
   onSubmit() {
 
+    if(this.form.get('userType')?.value === '') {
+      this.api.houseOwnerRegistration({
+        email: this.form.get('email')?.value,
+        password: this.form.get('password')?.value,
+        posswordConformation : this.form.get('passwordConformation')?.value,
+        firstName: this.form.get('firstName')?.value,
+        lastName: this.form.get('lastName')?.value,
+        address: this.form.get('address')?.value,
+        city: this.form.get('city')?.value,
+        conutry: this.form.get('country')?.value,
+        phoneNumber: this.form.get('phoneNumber')?.value,
+        userType: this.form.get('userType')?.value,
+        description: this.form.get('description')?.value,
+      }).subscribe((response: any) => {
+        this.router.navigate(['/']);
+      })
+    }
   }
 
 }
