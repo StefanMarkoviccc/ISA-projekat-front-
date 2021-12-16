960f7dcbea6b81cbdaccf92ae1bfe5eb82546de0
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-12-16 18:47:24 +0100

    #nesto

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -36,9 +36,6 @@ export class ApiService {
   deleteHouse(data: any){
     return this.http.delete(this.url + '/api/houses/' + data.id, this.generateHeader());
   }
-  deleteAdventure(data: any){
-    return this.http.delete(this.url + '/api/adventures/' + data.id, this.generateHeader());
-  }
   editHouse(data: any){
     return this.http.put(this.url + '/api/houses/' + data.id, data, this.generateHeader());
   }
@@ -57,12 +54,6 @@ export class ApiService {
   getHouses(data: any){
     return this.http.get(this.url + '/api/houses?search='+ data.search, this.generateHeader());
   }
-  getAdventure(data: any){
-    return this.http.get(this.url + '/api/adventures/' + data.id, this.generateHeader());
-  }
-  getAdventures(data: any){
-    return this.http.get(this.url + '/api/adventures', this.generateHeader());
-  }
   createBoat(data: any){
     return this.http.post(this.url + '/api/boats', data, this.generateHeader());
   }
@@ -78,19 +69,14 @@ export class ApiService {
   getBoats(data: any){
     return this.http.get(this.url + '/api/boats?search='+ data.search, this.generateHeader());
   }
+  getAdventures(data: any){
+    return this.http.get(this.url + '/api/adventures/', this.generateHeader());
+  }
+
   getHouseImages(data: any){
     return this.http.get(this.url + '/api/houseImages/' + data.id, this.generateHeader());
   }
-  getAdventureImages(data: any){
-    return this.http.get(this.url + '/api/adventureImages/' + data.id, this.generateHeader());
-  }
-  getInstuctorServices(data: any){
-    return this.http.get(this.url + '/api/instructorServices/' + data.id, this.generateHeader());
-  }
-  
-  editInstructorServices(data: any){
-    return this.http.put(this.url + '/api/instuctorServices/' + data.id, data, this.generateHeader());
-  }
+
   createImage(data: any) {
     return this.http.post(this.url + '/api/houseImages', data, this.generateHeader())
   }
