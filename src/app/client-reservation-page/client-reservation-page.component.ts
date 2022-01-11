b48075769ecc810c1f7235fd4767f145c5bfa61c
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2022-01-11 21:30:11 +0100

    client reservation

--- src/app/client-reservation-page/client-reservation-page.component.ts
+++ src/app/client-reservation-page/client-reservation-page.component.ts
@@ -10,111 +10,48 @@ import { ApiService } from '../api.service';
 })
 export class ClientReservationPageComponent implements OnInit {
   form: FormGroup;
-  selectedType: any;
-  id : any;
 
-  constructor(private formBuilder: FormBuilder, private api: ApiService, private router: Router,  private activatedRoute: ActivatedRoute) {
+
+  constructor(private formBuilder: FormBuilder, private api: ApiService, private router: Router, private activatedRoute: ActivatedRoute) {
     this.form = this.formBuilder.group({
-      email: ['', Validators.email],
-      password: ['', Validators.required],
-      passwordConfirmation: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required ],
-      address: ['', Validators.required],
-      city: ['', Validators.required],
-      country: ['', Validators.required],
-      phoneNumber: ['', Validators.required], 
-      userType: ['', Validators.required],
-      description: ['', Validators.required]})
+      dateTo: ['', Validators.required],
+      dateFrom: ['', Validators.required],
+      entity: ['', Validators.required],
+    });
 
-      this.form = this.formBuilder.group({
-        dateTo: ['', Validators.required],
-        dateFrom: ['', Validators.required],
-      });
+  
 
-      this.activatedRoute.queryParams.subscribe(params => {
-        this.id = params['id'];
-        console.log(this.id)
-      });
    }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-
-    console.log('Test', this.selectedType);
-
-    if(this.selectedType === 'HOUSE') {
-      this.api.houseOwnerRegistration({
-        email: this.form.get('email')?.value,
-        password: this.form.get('password')?.value,
-        posswordConformation : this.form.get('passwordConformation')?.value,
-        firstName: this.form.get('firstName')?.value,
-        lastName: this.form.get('lastName')?.value,
-        address: this.form.get('address')?.value,
-        city: this.form.get('city')?.value,
-        country: this.form.get('country')?.value,
-        phoneNumber: this.form.get('phoneNumber')?.value,
-        userType: this.selectedType,
-        description: this.form.get('description')?.value
+// prikazi samo izmedju dva datuma i da ovo bude combo box
+    if(this.form.get('entity')?.value=='House'){
+      this.api.getHouses({
+        
       }).subscribe((response: any) => {
-        this.router.navigate(['/home']);
+        this.router.navigate(['/houseView'])
       })
-    }else if(this.selectedType === 'BOAT'){
-      this.api.boatOwnerRegistration({
-        email: this.form.get('email')?.value,
-        password: this.form.get('password')?.value,
-        posswordConformation : this.form.get('passwordConformation')?.value,
-        firstName: this.form.get('firstName')?.value,
-        lastName: this.form.get('lastName')?.value,
-        address: this.form.get('address')?.value,
-        city: this.form.get('city')?.value,
-        country: this.form.get('country')?.value,
-        phoneNumber: this.form.get('phoneNumber')?.value,
-        userType: this.selectedType,
-        description: this.form.get('description')?.value,
-        }).subscribe((response: any) => {
-        this.router.navigate(['/home']);
+    }else if(this.form.get('entity')?.value=='Boat'){
+      this.api.getBoats({
+        
+      }).subscribe((response: any) => {
+        this.router.navigate(['/boatView'])
       })
-    }else if(this.selectedType === 'FISHING_INSTRUCTOR'){
-      this.api.fishingInstructorRegistration({
-        email: this.form.get('email')?.value,
-        password: this.form.get('password')?.value,
-        posswordConformation : this.form.get('passwordConformation')?.value,
-        firstName: this.form.get('firstName')?.value,
-        lastName: this.form.get('lastName')?.value,
-        address: this.form.get('address')?.value,
-        city: this.form.get('city')?.value,
-        country: this.form.get('country')?.value,
-        phoneNumber: this.form.get('phoneNumber')?.value,
-        userType: this.selectedType,
-        description: this.form.get('description')?.value,
-        }).subscribe((response: any) => {
-        this.router.navigate(['/home']);
+    }else if(this.form.get('entity')?.value=='Adventure'){
+      this.api.getAdventures({
+        
+      }).subscribe((response: any) => {
+        this.router.navigate(['/home'])
       })
-    }else{
-      this.api.clientRegistration({
-        email: this.form.get('email')?.value,
-        password: this.form.get('password')?.value,
-        posswordConformation : this.form.get('passwordConformation')?.value,
-        firstName: this.form.get('firstName')?.value,
-        lastName: this.form.get('lastName')?.value,
-        address: this.form.get('address')?.value,
-        city: this.form.get('city')?.value,
-        country: this.form.get('country')?.value,
-        phoneNumber: this.form.get('phoneNumber')?.value,
-        userType: this.selectedType,
-        description: this.form.get('description')?.value,
-
-        dateTo: this.form.get('dateTo')?.value,
-      dateFrom: this.form.get('dateFrom')?.value,
-
-        }).subscribe((response: any) => {
-        this.router.navigate(['/home']);
-      })
-  
-    
     }
+   // this.api.createBoatAvailabilityPeriod({
+     // dateTo: this.form.get('dateTo')?.value,
+      //dateFrom: this.form.get('dateFrom')?.value,
+    //}).subscribe((response: any) => {
+     // this.router.navigate(['/boatView']);
+    //})
   }
 }
